53b8b7eb3493ceaa46bf38bf6f9c09e685c05eea
AlejandroGR <alejandrogemesrubio@cdpjosecabrera.es>
2022-10-20 00:05:10 +0200

    Este comit es del proyecto de estructuras basicas de Java, ultima cosa que hay son los ejercicios de Bucles

---
+++ src/Vista/Main.java
@@ -0,0 +1,26 @@
+ package Vista;
+
+import ejercicios.EjerciciosBucles;
+import ejercicios.EjerciciosSentenciasCondicionales;
+import ejercicios.EjerciciosSentenciasCondicionalesP2;
+import estudioCondiciones.EstudioCondiciones;
+import estudioEstructurasCondicionales.EstudioEstructurasCondicionales;
+import estudioEstructurasRepeticion.EstudioEstructurasRepeticion;
+import estudioOperadoresAritmeticos.EstudioOperadoresAritmeticos;
+import estudioTipoDeDatos.EstudioTipoDeDatos;
+import tiposDatos.EstudioDatos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//EstudioTipoDeDatos estudioTipoDeDatos=new EstudioTipoDeDatos();
+	//EstudioOperadoresAritmeticos aritmeticos=new EstudioOperadoresAritmeticos();	
+	//EstudioEstructurasCondicionales condiciones=new EstudioEstructurasCondicionales();
+	//EjerciciosSentenciasCondicionales ejercicios=new EjerciciosSentenciasCondicionales();	
+	//EjerciciosSentenciasCondicionalesP2 ejercicios=new EjerciciosSentenciasCondicionalesP2();
+	//EstudioEstructurasRepeticion bucles=new EstudioEstructurasRepeticion();
+	EjerciciosBucles bucles=new EjerciciosBucles();
+	}
+//end class
+}
